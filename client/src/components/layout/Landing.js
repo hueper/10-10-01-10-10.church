7e3f82a6707696bd02b6db2a43edfe3d39f4a936
hueper <github@localinit.com>
2020-04-05 16:32:48 +0200

    initial commit

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "80vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="flow-text col s12 center-align">
+            <h4 style={{letterSpacing:'0.5rem'}}>
+              10-10-01-10-10
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              Streaming churchy.
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable black accent-3"
+              >
+                Convert
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect yellow black-text"
+              >
+                Pray
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              className="col s5 brand-logo center black-text"
+            >
+              🇻🇦️
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
